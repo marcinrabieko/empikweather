d983c4ec891787018f59b292d72ed9da6e66f01c
Marcin Rabieko <marcin.rabieko@itcraft.pl>
2023-12-10 21:26:48 +0100

    Initialised project

---
+++ Empik Weather/Empik Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Empik Weather
+//
+//  Created by Marcin Rabieko on 10/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
